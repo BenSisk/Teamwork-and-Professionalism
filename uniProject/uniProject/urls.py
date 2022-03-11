80b39d73d42d0894cbc28ca7688c5f689d5d94d5
Anthony Donnelly <amzo1337@gmail.com>
2022-03-11 05:57:23 -0500

    Add a favicon.ico and update settings and template to reflect this

--- uniProject/uniProject/urls.py
+++ uniProject/uniProject/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import include, path
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('', include('adminSection.urls')),
+    path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
