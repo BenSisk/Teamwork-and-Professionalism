fdc97aac87f76499754ee975018265d8214b5125
iamloxton <iamdevara@gmail.com>
2022-04-15 08:09:29 -0400

    Resolve conflict of file forms.py

--- uniProject/adminSection/forms.py
+++ uniProject/adminSection/forms.py
@@ -11,3 +11,8 @@ class UserLoginForm(AuthenticationForm):
 
     username = forms.TextInput(attrs={'class': 'form-control', 'placeholder': '', 'id': 'hello'})
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '', 'id': 'hi'}))
+
+class DocumentForm(forms.Form):
+    title = forms.CharField(label='Project Title', max_length=30)
+    desc = forms.CharField(widget=forms.Textarea(attrs={'title': 'Project Description'}), label='Project Description', max_length=200)
+    docfile = forms.FileField(label='Select a file')
