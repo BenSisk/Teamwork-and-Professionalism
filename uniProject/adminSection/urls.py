3279699af60b2bdf96235d3bc88a48c02237baee
iamloxton <iamdevara@gmail.com>
2022-04-15 08:12:37 -0400

    Manually resolve conflict for urls.py

--- uniProject/adminSection/urls.py
+++ uniProject/adminSection/urls.py
@@ -1,4 +1,4 @@
-from django.urls import include, path
+from django.urls import path
 from . import views
 from .forms import UserLoginForm
 
@@ -7,5 +7,8 @@ urlpatterns = [
  path('', views.index, name="index"),
  path('login/', views.Login.as_view(template_name='login.html', authentication_form=UserLoginForm), name='login'),
  path('logout/', views.logout_view, name='logout'),
- path('admintest/', views.admin_view, name='admin'),
+ path('admin/', views.admin_view, name='admin'),
+ path('upload/', views.upload_view, name='upload'),
+ path('account/', views.account_view, name='account'),
 ]
+
