6800e0dc92230981cd0468426dfe1a3e1068b10e
Anthony Donnelly <amzo1337@gmail.com>
2022-04-17 07:03:07 -0400

    Add files that where recreated during conflict resolve

---
+++ uniProject/crawler/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+NEW_PAGE = [
+    ('Yes', True),
+    ('No', False),
+    ]
+
+class SearchCriteria(forms.Form):
+	searchTerm = forms.CharField(help_text="Material to search for")
+
+	newPage = forms.CharField(label="Fetch New Page:", widget=forms.Select(choices=NEW_PAGE))
+
+
+	def modify_search_query(self):
+		data = self.cleaned_data['searchTerm']
+		# add in dimensions into  search
+		searchString = data + "+%28L%29+%28T%29+%28W%29"
+
+		return searchString
