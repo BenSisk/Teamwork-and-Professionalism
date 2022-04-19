0cd995ad8ed845cafbbcef93baefbfbff52e6f84
Anthony Donnelly <amzo1337@gmail.com>
2022-04-19 09:14:53 -0400

    resolve final conflicts

---
+++ uniProject/pricePrediction/api_keys.py
@@ -0,0 +1,11 @@
+apikeys = {
+    "steel-votingensemble" : "API_KEY",
+    "plywood-votingensemble" : "API_KEY",
+    "steel-elasticnet" : "API_KEY",
+    "plywood-elasticnet" : "API_KEY"
+    }
+
+ipaddress = {
+    "votingensemble" : "IP",
+    "elasticnet" : "IP"
+}
